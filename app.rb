16982c7d5cd8c7d658b789d958f4b0b3cd2864ea
Илья <76055136+ILYAKOINOV@users.noreply.github.com>
2020-12-27 13:48:26 +0300

    init app

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	@error = 'something wrong!'
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# хэш
+	hh = { 
+		:username => 'Введите имя',
+		:phone => 'Введите номер телефона',
+		:datetime => 'Введите дату и время'}
+		# для каждой пары ключ-значение
+		#hh.each do |key, value|
+		    # если параметр пуст 
+		#	if params[key] == ''
+			# переменной error присвоить value из хэша hh
+			# (а value из хэша hh это сообщение об ошибке)
+			# т.е. переменной error присвоить сообщение об ошибке
+		#		@error = hh[key]
+			#Вернуть представление visit	
+		#		return erb :visit
+		#	end	
+		#end
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")	
+
+	if @error != ''
+		return erb :visit
+	end
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+end	
