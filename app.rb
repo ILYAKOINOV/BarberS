1770cbfbeb29b64c67a3af240d657050b09b2730
Илья <76055136+ILYAKOINOV@users.noreply.github.com>
2020-12-27 16:39:49 +0300

    Copy Roman version

--- app.rb
+++ app.rb
@@ -3,9 +3,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end 
+	end
+
+end
+
+def get_db
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
+end
+
+before do
+	db = get_db
+	@barbers = db.execute 'select * from Barbers'
+end
+
 configure do
-	@db = SQLite3::Database.new 'barbershop.db'
-	@db.execute 'CREATE TABLE IF NOT EXISTS
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
 		"Users"
 		(
 			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,6 +40,15 @@ configure do
 			"barber" TEXT,
 			"color" TEXT
 		)'
+
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Barbers"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"name" TEXT
+		)'
+
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
@@ -22,7 +56,6 @@ get '/' do
 end
 
 get '/about' do
-	@error = 'something wrong!'
 	erb :about
 end
 
@@ -38,30 +71,38 @@ post '/visit' do
 	@barber = params[:barber]
 	@color = params[:color]
 
-	# хэш
-	hh = { 
-		:username => 'Введите имя',
-		:phone => 'Введите номер телефона',
-		:datetime => 'Введите дату и время'}
-		# для каждой пары ключ-значение
-		#hh.each do |key, value|
-		    # если параметр пуст 
-		#	if params[key] == ''
-			# переменной error присвоить value из хэша hh
-			# (а value из хэша hh это сообщение об ошибке)
-			# т.е. переменной error присвоить сообщение об ошибке
-		#		@error = hh[key]
-			#Вернуть представление visit	
-		#		return erb :visit
-		#	end	
-		#end
+	# хеш
+	hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время' }
 
-	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")	
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
 
 	if @error != ''
 		return erb :visit
 	end
 
-	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+	db = get_db
+	db.execute 'insert into
+		Users
+		(
+			username,
+			phone,
+			datestamp,
+			barber,
+			color
+		)
+		values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
-end	
+	erb "<h2>Спасибо, вы записались!</h2>"
+
+end
+
+get '/showusers' do
+	db = get_db
+
+	@results = db.execute 'select * from Users order by id desc'
+
+	erb :showusers
+end
+
